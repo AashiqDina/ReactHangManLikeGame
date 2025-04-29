70f9517fb0c1663d21b24720c750ff15c679160d
AashiqDina <98742552+AashiqDina@users.noreply.github.com>
2025-04-29 20:38:12 +0100

    Small Update

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header(){
     return (
         <header>
-            <h1>Save the Languages</h1>
-            <p>Try to guess the word in under 8 attempts!</p>
+            <h1>Save the Computer Parts</h1>
+            <p>Try to guess the word in under 10 attempts!</p>
         </header>
     )
 }
